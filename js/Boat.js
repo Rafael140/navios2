819c95164e6d1661b00df79a194c3c2811b648a6
Rafael140 <94143650+Rafael140@users.noreply.github.com>
2022-02-25 18:55:21 -0300

    Add files via upload

--- js/Boat.js
+++ js/Boat.js
@@ -1,41 +1,31 @@
 class Boat {
+  constructor(x, y, width, height, boatPos) {
+  
+    this.body = Bodies.rectangle(x, y, width, height);
+    this.width = width;
+    this.height = height;
+    this.boatPosition = boatPos;
+    this.image = loadImage("./assets/boat.png");
 
-    constructor(x, y, width, height, boatPos){
+    World.add(world, this.body);
+  }
 
-        this.body = Bodies.rectangle(x, y, width, height);
-        this.width = width;
-        this.height = height;
+  remove(index) {
+    setTimeout(() => {
+      Matter.World.remove(world, boats[index].body);
+      delete boats[index];
+    }, 2000);
+  }
 
-        this.image  = loadImage("/assets/boat.png");
-        this.boatPosition = boatPos;
-        World.add(world, this.body);
+  display() {
+    var angle = this.body.angle;
+    var pos = this.body.position;
 
-    }
-
-
-    display(){
-
-        push();
-        translate(this.body.position.x, this.body.position.y);
-        rotate(this.body.angle);
-        imageMode(CENTER);
-        image(this.image, 0, this.boatPosition, this.width, this.height);
-        pop();
-
-    }
-
-    remove(index){
-
-        setTimeout(() =>{
-
-            Matter.World.remove(world, boats[index].body);
-            delete boats[index];
-
-        }, 2000);
-
-
-    }
-
-
-
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, this.boatPosition, this.width, this.height);
+    pop();
+  }
 }
--- js/Cannon.js
+++ js/Cannon.js
@@ -1,35 +1,30 @@
 class Cannon {
-
   constructor(x, y, width, height, angle) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.angle = angle;
-    
-    this.cannonBase = loadImage("./assets/cannonBase.png");
-    this.cannon = loadImage("./assets/canon.png")
+    this.cannon_image = loadImage("assets/canon.png");
+    this.cannon_base = loadImage("assets/cannonBase.png");
   }
-  
-  display(){
-
-    if(keyIsDown(RIGHT_ARROW) && this.angle <= 50){
-      this.angle += 1
+  display() {
+    if (keyIsDown(RIGHT_ARROW) && this.angle<70  ) {
+      this.angle += 1;
     }
-    if(keyIsDown(LEFT_ARROW) && this.angle >= -40){
+
+    if (keyIsDown(LEFT_ARROW) && this.angle>-30 ) {
       this.angle -= 1;
     }
 
+
     push();
-    translate(this.x, this.y)
+    translate(this.x, this.y);
     rotate(this.angle);
     imageMode(CENTER);
-    image(this.cannon, 0, 0, this.width, this.height)
+    image(this.cannon_image, 0, 0, this.width, this.height);
     pop();
-
-    image(this.cannonBase,65,30,200,200);
+    image(this.cannon_base, 70, 20, 200, 200);
     noFill();
-    
   }
-
 }
